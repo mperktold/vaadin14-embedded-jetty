9838564d411fad9c4cda706b174977ac7da93671
Matthias Perktold <tias251@gmail.com>
2021-05-14 16:40:38 +0200

    Upgrade to Java 11, Vaadin 19 and Jetty 10

--- src/main/java/com/vaadin/starter/skeleton/Main.java
+++ src/main/java/com/vaadin/starter/skeleton/Main.java
@@ -2,15 +2,14 @@ package com.vaadin.starter.skeleton;
 
 import com.vaadin.flow.server.VaadinServlet;
 import com.vaadin.flow.server.startup.ServletContextListeners;
-import org.eclipse.jetty.annotations.AnnotationConfiguration;
-import org.eclipse.jetty.server.Server;
-import org.eclipse.jetty.util.resource.Resource;
-import org.eclipse.jetty.webapp.*;
-import org.eclipse.jetty.websocket.jsr356.server.deploy.WebSocketServerContainerInitializer;
-import org.jetbrains.annotations.NotNull;
 
 import java.net.MalformedURLException;
 import java.net.URL;
+
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.util.resource.Resource;
+import org.eclipse.jetty.webapp.WebAppContext;
+import org.jetbrains.annotations.NotNull;
 
 /**
  * Run {@link #main(String[])} to launch your app in Embedded Jetty.
@@ -44,7 +43,6 @@ public final class Main {
         context.setConfigurationDiscovered(true);
         context.getServletContext().setExtendedListenerTypes(true);
         context.addEventListener(new ServletContextListeners());
-        WebSocketServerContainerInitializer.initialize(context); // fixes IllegalStateException: Unable to configure jsr356 at that stage. ServerContainer is null
 
         int port = 8080;
         if (args.length >= 1) {
@@ -52,8 +50,6 @@ public final class Main {
         }
         server = new Server(port);
         server.setHandler(context);
-        final Configuration.ClassList classlist = Configuration.ClassList.setServerDefault(server);
-        classlist.addBefore(JettyWebXmlConfiguration.class.getName(), AnnotationConfiguration.class.getName());
         server.start();
 
         System.out.println("\n\n=================================================\n\n" +
--- src/main/java/com/vaadin/starter/skeleton/MainView.java
+++ src/main/java/com/vaadin/starter/skeleton/MainView.java
@@ -6,6 +6,7 @@ import com.vaadin.flow.component.button.ButtonVariant;
 import com.vaadin.flow.component.dependency.CssImport;
 import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.page.AppShellConfigurator;
 import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.Route;
 import com.vaadin.flow.server.PWA;
@@ -14,10 +15,10 @@ import com.vaadin.flow.server.PWA;
  * The main view contains a button and a click listener.
  */
 @Route("")
-@PWA(name = "Project Base for Vaadin", shortName = "Project Base", enableInstallPrompt = false)
+@PWA(name = "Project Base for Vaadin", shortName = "Project Base")
 @CssImport("./styles/shared-styles.css")
 @CssImport(value = "./styles/vaadin-text-field-styles.css", themeFor = "vaadin-text-field")
-public class MainView extends VerticalLayout {
+public class MainView extends VerticalLayout implements AppShellConfigurator {
 
     public MainView() {
         // Use TextField for standard text input
